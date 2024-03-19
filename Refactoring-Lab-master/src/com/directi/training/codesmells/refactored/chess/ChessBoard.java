8cdfd69556583651cfaeca3fdea832fc2e7e23a1
Tunyarat Nitihirunpattana <113578736+TongTunyarat@users.noreply.github.com>
2024-03-19 17:25:19 +0700

    test sonar cloud

--- Refactoring-Lab-master/src/com/directi/training/codesmells/refactored/chess/ChessBoard.java
+++ Refactoring-Lab-master/src/com/directi/training/codesmells/refactored/chess/ChessBoard.java
@@ -1,95 +1,47 @@
-package com.directi.training.codesmells.refactored.chess;
+package com.directi.training.codesmells.smelly.chess;
 
-import com.directi.training.codesmells.refactored.Color;
-import com.directi.training.codesmells.refactored.Direction;
-import com.directi.training.codesmells.refactored.Position;
-import com.directi.training.codesmells.refactored.pieces.*;
+import com.directi.training.codesmells.smelly.Color;
+import com.directi.training.codesmells.smelly.Direction;
+import com.directi.training.codesmells.smelly.Position;
+import com.directi.training.codesmells.smelly.pieces.King;
+import com.directi.training.codesmells.smelly.pieces.Knight;
+import com.directi.training.codesmells.smelly.pieces.Pawn;
+import com.directi.training.codesmells.smelly.pieces.Piece;
 
 public class ChessBoard
 {
-    // Fixes magic number code smell. Although this const is not supposed to be changed, but this avoids a typo
-    private static final int BOARD_SIZE = 8;
     private final Cell[][] _board;
-    private boolean _kingDead; //On fixing feature envy code smell, this should be made private and getBoard method be removed
+    public boolean _kingDead;
+    public Player player1, player2;
 
     public ChessBoard()
     {
-        _board = new Cell[BOARD_SIZE][BOARD_SIZE];
+        _board = new Cell[8][8];
         initBoard();
-        resetBoard(); // Added later
+
     }
 
     private void initBoard()
     {
-        for (int row = 0; row < BOARD_SIZE; row++) {
-            for (int column = 0; column < BOARD_SIZE; column++) {
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 8; column++) {
                 Color color = ((row + column) % 2 == 0) ? Color.WHITE : Color.BLACK;
                 _board[row][column] = new Cell(color);
             }
         }
     }
 
-    //Code smells solved: Duplicate code (within method) and long method.
-    public void resetBoard()
+    public Cell[][] getBoard()
     {
-        placePieces(Color.WHITE);
-        placePieces(Color.BLACK);
-        _kingDead = false;
-    }
-
-    private void placePieces(Color color)
-    {
-        int pawnsRow, otherPiecesRow;
-        switch (color) {
-            case WHITE:
-                pawnsRow = BOARD_SIZE - 2;
-                otherPiecesRow = BOARD_SIZE - 1;
-                break;
-            case BLACK:
-                pawnsRow = 1;
-                otherPiecesRow = 0;
-                break;
-            default:
-                System.err.println("Unexpected color passed to placePieces.");
-                return;
-        }
-        placeOtherPieces(otherPiecesRow, color);
-        placePawns(pawnsRow, color);
-    }
-
-    private void placePawns(int row, Color color)
-    {
-        for (int column = 0; column < BOARD_SIZE; column++) {
-            _board[row][column].setPiece(new Pawn(color));
-        }
-    }
-
-    private void placeOtherPieces(int row, Color color)
-    {
-        assert BOARD_SIZE == 8;
-        for (int column = 0; column < BOARD_SIZE; column++) {
-            Piece piece = null;
-            if (column == 0 || column == BOARD_SIZE - 1) {
-                piece = new Rook(color);
-            } else if (column == 1 || column == BOARD_SIZE - 2) {
-                piece = new Knight(color);
-            } else if (column == 2 || column == BOARD_SIZE - 3) {
-                piece = new Bishop(color);
-            } else if (column == 3) {
-                piece = new King(color);
-            } else if (column == 4) {
-                piece = new Queen(color);
-            }
-            _board[row][column].setPiece(piece);
-        }
+        return _board;
     }
 
     private boolean isPositionOutOfBounds(Position position)
     {
         return (position.getRow() < 0
-                || position.getRow() >= BOARD_SIZE
+                || position.getRow() >= 8
                 || position.getColumn() < 0
-                || position.getColumn() >= BOARD_SIZE);
+                || position.getColumn() >= 8);
     }
 
     public boolean isEmpty(Position position)
@@ -102,42 +54,67 @@ public class ChessBoard
         return _board[position.getRow()][position.getColumn()];
     }
 
-    //Dead-Code Code Smell fixed by removing getPlayerName and printMove methods (and also toString of Position),
-    // as well as player 1 and player 2 fields.
-
-    //Fixed Duplicate Code across methods of same class Code-Smell by calling isEmpty method
     public Piece getPiece(Position position)
     {
-        return isEmpty(position) ? null : getCell(position).getPiece();
+        return (isPositionOutOfBounds(position) || getCell(position).isEmpty())
+                ? null
+                : getCell(position).getPiece();
     }
 
-    //Fixed long parameter list code smell: Pass the object itself instead of passing its data.
-    // (isValidMove, movePiece, updateIsKingDead, updatePawnStatus)
-    public boolean isValidMove(Position from, Position to)
+    public String getPlayerName(Position position)
     {
+        if (isPositionOutOfBounds(position))
+            return null;
+        Color color = getCell(position).getPiece().getColor();
+        if (color == player1.getColor()) {
+            return player1.getName();
+        } else {
+            return player2.getName();
+        }
+    }
+
+    private void printMove(Position from, Position to)
+    {
+        System.out.println(getPlayerName(from) + " moved " + getPiece(from) + " from " + from + " to " + to);
+        if (getPiece(from).getColor() != getPiece(to).getColor()) {
+            System.out.println("And has captured " + getPiece(to) + " of " + getPlayerName(to));
+        }
+    }
+
+    public boolean isValidMove(int fromRow, int fromColumn, int toRow, int toColumn)
+    {
+        Position from = new Position(fromRow, fromColumn);
+        Position to = new Position(toRow, toColumn);
         return !from.equals(to)
-               && !(isPositionOutOfBounds(from) || isPositionOutOfBounds(to))
-               && !isEmpty(from)
-               && (isEmpty(to) || getPiece(from).getColor() != getPiece(to).getColor())
-               && getPiece(from).isValidMove(from, to)
-               && hasNoPieceInPath(from, to)
-               && (!(getPiece(from) instanceof Pawn) || isValidPawnMove(from, to));
+                && !(isPositionOutOfBounds(from) || isPositionOutOfBounds(to))
+                && !isEmpty(from)
+                && (isEmpty(to) || getPiece(from).getColor() != getPiece(to).getColor())
+                && getPiece(from).isValidMove(from, to)
+                && hasNoPieceInPath(from, to)
+                && (!(getPiece(from) instanceof Pawn) || isValidPawnMove(from, to));
     }
 
     private boolean hasNoPieceInPath(Position from, Position to)
     {
         if (getPiece(from) instanceof Knight)
             return true;
-        if (!MoveUtil.isStraightLineMove(from, to))
+        if (!isStraightLineMove(from, to))
             return false;
         Direction direction = new Direction(cappedCompare(to.getRow(), from.getRow()), cappedCompare(to.getColumn(), from.getColumn()));
-        from = from.translatedPosition(direction);
+        from = translatedPosition(from, direction);
         while (!from.equals(to)) {
             if (!isEmpty(from))
                 return false;
-            from = from.translatedPosition(direction);
+            from = translatedPosition(from, direction);
         }
         return true;
+    }
+
+    private boolean isStraightLineMove(Position from, Position to)
+    {
+        return Math.abs(from.getRow() - to.getRow()) == Math.abs(from.getColumn() - to.getColumn())
+                || from.getRow() == to.getRow()
+                || from.getColumn() == to.getColumn();
     }
 
     private int cappedCompare(int x, int y)
@@ -145,18 +122,25 @@ public class ChessBoard
         return Math.max(-1, Math.min(1, Integer.compare(x, y)));
     }
 
-    public void movePiece(Position from, Position to)
+    private Position translatedPosition(Position from, Direction direction)
     {
-        updateIsKingDead(to);
+        return new Position(from.getRow() + direction.getRowOffset(), from.getColumn() + direction.getColumnOffset());
+    }
+
+    public void movePiece(int fromRow, int fromColumn, int toRow, int toColumn)
+    {
+        Position from = new Position(fromRow, fromColumn);
+        Position to = new Position(toRow, toColumn);
+        updateIsKingDead(toRow, toColumn);
         if (!getCell(to).isEmpty())
             getCell(to).removePiece();
         getCell(to).setPiece(getPiece(from));
         getCell(from).removePiece();
     }
 
-    private void updateIsKingDead(Position positionBeingMovedTo)
+    private void updateIsKingDead(int row, int column)
     {
-        if (getPiece(positionBeingMovedTo) instanceof King) {
+        if (getPiece(new Position(row, column)) instanceof King) {
             _kingDead = true;
         }
     }
@@ -172,7 +156,7 @@ public class ChessBoard
 
         boolean opponentPieceAtForwardLeft = !isEmpty(forwardLeft) && getPiece(forwardLeft).getColor() != pawnColor;
         boolean opponentPieceAtForwardRight = !isEmpty(forwardRight) && getPiece(forwardRight).getColor() != pawnColor;
-        boolean atInitialPosition = from.getRow() == ((pawnColor == Color.BLACK) ? 1 : BOARD_SIZE - 2);
+        boolean atInitialPosition = from.getRow() == ((pawnColor == Color.BLACK) ? 1 : 6);
 
         return pawn.isValidMoveGivenContext(from, to, atInitialPosition, opponentPieceAtForwardLeft, opponentPieceAtForwardRight);
     }
@@ -186,16 +170,16 @@ public class ChessBoard
     public String toString()
     {
         StringBuilder stringBuilder = new StringBuilder(" ");
-        for (int column = 0; column < BOARD_SIZE; column++) {
+        for (int column = 0; column < 8; column++) {
             stringBuilder.append("  ")
                     .append(column + 1)
                     .append("  ");
         }
         stringBuilder.append("\n");
 
-        for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int row = 0; row < 8; row++) {
             stringBuilder.append(row + 1);
-            for (int column = 0; column < BOARD_SIZE; column++) {
+            for (int column = 0; column < 8; column++) {
                 stringBuilder.append(" ")
                         .append(_board[row][column])
                         .append(" ");
